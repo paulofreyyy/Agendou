6b0aea374f163fbc383a6b8693bfaad00c8b0d8d
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2024-10-21 17:54:22 -0300

    feat: organizando arquitetura do projeto

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { App } from './App.tsx'
 import './index.css'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>,
 )
